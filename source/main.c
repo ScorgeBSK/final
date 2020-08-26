9f3ed64b846941532e15552940d5b4234b8330db
= <tlam036@ucr.edu>
2020-08-26 02:06:07 -0700

    added headers

--- source/main.c
+++ source/main.c
@@ -10,14 +10,58 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "usart.h"
+#include "timer.h"
 #endif
+
+enum States {off, on} state;
+unsigned char led = 0;
+void tick(){
+	switch(state){
+		case off:
+			state = on;
+			break;
+		case on:
+			state = off;
+			break;
+		default:
+			state = off;
+			break;
+	}
+
+	switch(state){
+		case off:
+			led = 0;
+			if(USART_IsSendReady()){
+				USART_Send(led);
+			}
+			break;
+		case on:
+			led = 1;
+			if(USART_IsSendReady()){
+				USART_Send(led);
+			}
+			break;
+		default:
+			break;
+	}
+
+	PORTB = led;
+}
 
 int main(void) {
     /* Insert DDR and PORT initializations */
 
+    initUSART();
+
+    TimerSet(1000);
+    TimerOn();
+
     /* Insert your solution below */
     while (1) {
-
+    	tick();
+	while(!TimerFlag) {};
+	TimerFlag = 0;
     }
     return 1;
 }
